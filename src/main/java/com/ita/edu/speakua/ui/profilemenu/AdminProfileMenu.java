cc9e0a79fd8969f8a27d8eca844ea0b6be6579d0
Coffezka <42944711+Coffezka@users.noreply.github.com>
2022-12-08 12:42:47 +0200

    Init Admin & User profile menu objects and refactor

---
+++ src/main/java/com/ita/edu/speakua/ui/profilemenu/AdminProfileMenu.java
@@ -0,0 +1,10 @@
+package com.ita.edu.speakua.ui.profilemenu;
+
+import org.openqa.selenium.WebDriver;
+
+public class AdminProfileMenu extends UserProfileMenu{
+
+    public AdminProfileMenu(WebDriver driver) {
+        super(driver);
+    }
+}
