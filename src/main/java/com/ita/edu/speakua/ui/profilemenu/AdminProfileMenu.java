a903f28c6946e5b6e82b100bfc898e777205f832
bohdan-khudio <bahdan510@gmail.com>
2022-12-11 16:49:49 +0200

    add element into AddTaskPage

--- src/main/java/com/ita/edu/speakua/ui/profilemenu/AdminProfileMenu.java
+++ src/main/java/com/ita/edu/speakua/ui/profilemenu/AdminProfileMenu.java
@@ -6,15 +6,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class AdminProfileMenu extends UserProfileMenu{
-@FindBy(xpath = "//span[contains(text(), 'Контент')]")
-private WebElement contentButton;
-@FindBy(xpath = "//span[contains(text(), 'Челенджі')]")
-private WebElement challengeButton;
-@FindBy(xpath = "//a[contains(text(), 'Челенджі')]")
-private WebElement challengesButton;
-@FindBy(xpath = "//a[contains(text(), 'Завдання')]")
-private WebElement tasksButton;
+public class AdminProfileMenu extends UserProfileMenu {
+    @FindBy(xpath = "//span[contains(text(), 'Контент')]")
+    private WebElement contentButton;
+    @FindBy(xpath = "//span[contains(text(), 'Челенджі')]")
+    private WebElement challengeButton;
+    @FindBy(xpath = "//a[contains(text(), 'Челенджі')]")
+    private WebElement challengesButton;
+    @FindBy(xpath = "//a[contains(text(), 'Завдання')]")
+    private WebElement tasksButton;
 
     public TasksPage openTasksPage() {
 
@@ -40,5 +40,8 @@ private WebElement tasksButton;
         action.click(challengesButton);
         return new ChallengePage(driver);
     }
-    public AdminProfileMenu(WebDriver driver) {super(driver);}
+
+    public AdminProfileMenu(WebDriver driver) {
+        super(driver);
+    }
 }
--- src/test/java/com/ita/edu/speakua/ui/contentTest.java
+++ src/test/java/com/ita/edu/speakua/ui/contentTest.java
@@ -39,7 +39,8 @@ public class contentTest extends BaseTestRunner {
         new HomePage(driver)
                 .openAdminProfileMenu()
                 .openTasksPage()
-                .addTaskButton();
+                .clickAddTask()
+                .enterTaskDescription("12345");
 
     }
 
