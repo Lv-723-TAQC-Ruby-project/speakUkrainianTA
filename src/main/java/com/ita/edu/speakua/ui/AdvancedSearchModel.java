704f9f358ffe8098c267279e7d61c26276590449
Coffezka <42944711+Coffezka@users.noreply.github.com>
2022-12-12 17:43:37 +0200

    Update AdvancedSearchTest

--- src/main/java/com/ita/edu/speakua/ui/AdvancedSearchModel.java
+++ src/main/java/com/ita/edu/speakua/ui/AdvancedSearchModel.java
@@ -1,5 +1,6 @@
 package com.ita.edu.speakua.ui;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -16,6 +17,16 @@ public class AdvancedSearchModel extends BasePO{
     public AdvancedSearchModel clickCenter() {
         centerButton.click();
         return this;
+    }
+    public boolean isAdvancedSearchOpen() {
+        try {
+            wait.visibility(By.xpath("//div[contains(text(), 'Розширений пошук')]"));
+            sleep(3);
+            driver.findElement(By.xpath("//div[contains(text(), 'Розширений пошук')]"));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
 }
--- src/test/java/com/ita/edu/speakua/ui/AdvancedSearchTest.java
+++ src/test/java/com/ita/edu/speakua/ui/AdvancedSearchTest.java
@@ -1,6 +1,7 @@
 package com.ita.edu.speakua.ui;
 
 import com.ita.edu.speakua.ui.runners.BaseTestRunner;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -11,26 +12,18 @@ public class AdvancedSearchTest extends BaseTestRunner {
     @BeforeClass
     public void setUp() {
         setDriver();
-        new HomePage(driver)
-                .openGuestProfileMenu()
-                .openLoginModel()
-                .enterEmail(configProperties.getAdminEmail())
-                .enterPassword(configProperties.getAdminPassword())
-                .clickLogin();
-
     }
-
-    @BeforeMethod
-    public void goHomePage() {
-        driver.get(configProperties.getBaseWebUrl());
-    }
-
-
 
 
     @Test
     public void checkAdvancedSearch() {
-
+       HomePage Page = new HomePage(driver);
+       boolean check  = Page.openAdvancedSearch().isAdvancedSearchOpen();
+       Assert.assertTrue(check);
+       System.out.println("True");
+       check = Page.openAdvancedSearch().isAdvancedSearchOpen();
+       Assert.assertFalse(check);
+       System.out.println("False");
     }
 
 
