98e93ea4fc36b5e7a1a8f438cc93eb2800d993f3
sofiy <sofiya.ometiukh@gmail.com>
2022-12-14 23:04:54 +0200

    upgrade ClubTests

--- src/test/java/com/ita/edu/speakua/ui/ClubTests.java
+++ src/test/java/com/ita/edu/speakua/ui/ClubTests.java
@@ -2,11 +2,11 @@ package com.ita.edu.speakua.ui;
 
 import com.ita.edu.speakua.ui.Pages.ClubsPO.AddClubModel;
 import com.ita.edu.speakua.ui.runners.BaseTestRunner;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class ClubTests extends BaseTestRunner {
 
@@ -39,33 +39,19 @@ public class ClubTests extends BaseTestRunner {
                 .enterPhoneNumber("0932584213")
                 .clickNextStep();
 
-        //Verify that the ‘Опис’ text field is filled in with valid data
 
-        boolean withValidValueUkrainianAndEnglishLetters = addClubModel.enterClubDescription("'Education', 'students', 'Школа' 'балету' ")
-                .successMessageDisplayed();
-
-        Assert.assertTrue(withValidValueUkrainianAndEnglishLetters, "Error message doesn't display");
-
-        boolean withValidValueNumbers = addClubModel.enterClubDescription("1234567890123456789012345678901234567890")
-                .successMessageDisplayed();
-
-        Assert.assertTrue(withValidValueNumbers, "Error message doesn't display");
-
-        boolean withValidValueSpecialCharacters = addClubModel.enterClubDescription("!#$%&'()*+,-./:;<=>?@[]^_`{}~%^$#)&&^^(_&($%^#@!")
-                .successMessageDisplayed();
-
-        Assert.assertTrue(withValidValueSpecialCharacters, "Error message doesn't display");
-
-        boolean withInappropriateDescription = addClubModel.enterClubDescription("ё ы э ъ")
-                .isErrorMessageDisplayed("Опис гуртка не може містити російські літери");
-
-        Assert.assertTrue(withInappropriateDescription, "Error message is displayed");
-
-        boolean withShortDescription = addClubModel.enterClubDescription("В гурток ходить 3 людини")
-                .isErrorMessageDisplayed("Опис гуртка може містити від 40 до 1500 символів.");
-
-        Assert.assertTrue(withShortDescription, "Error message is displayed");
-
+        SoftAssert softAssert = new SoftAssert();
+        //TUA-176
+        softAssert.assertTrue(addClubModel.enterClubDescription("В гурток ходить 3 людини").isErrorMessageDisplayed("Опис гуртка може містити від 40 до 1500 символів."));
+        //TUA-178
+        softAssert.assertTrue(addClubModel.enterClubDescription("ё ы э ъ").isErrorMessageDisplayed("Опис гуртка не може містити російські літери"));
+        softAssert.assertTrue(addClubModel.enterClubDescription("ё ы э ъ").isErrorMessageDisplayed("Некоректний опис гуртка"));
+        //TUA-173
+        softAssert.assertTrue(addClubModel.enterClubDescription("'Education', 'students', 'Школа' 'балету'").successMessageDisplayed());
+        softAssert.assertTrue(addClubModel.enterClubDescription("1234567890123456789012345678901234567890").successMessageDisplayed());
+        softAssert.assertTrue(addClubModel.enterClubDescription("!#$%&'()*+,-./:;<=>?@[]^_`{}~%^$#)&&^^(_&($%^#@!").successMessageDisplayed());
+        addClubModel.finishAddingCenter();
+        softAssert.assertTrue(addClubModel.completeButtonEnabled());
     }
 
     @AfterClass
