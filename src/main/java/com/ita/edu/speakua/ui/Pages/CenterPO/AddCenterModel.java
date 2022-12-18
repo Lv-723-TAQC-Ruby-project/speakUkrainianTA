47eaef20945cdac8c9f39758e706343d54e4c6b3
simpleUA <a.lipskiy@yahoo.com>
2022-12-18 12:07:26 +0200

    fix addCenterTest

--- src/main/java/com/ita/edu/speakua/ui/Pages/CenterPO/AddCenterModel.java
+++ src/main/java/com/ita/edu/speakua/ui/Pages/CenterPO/AddCenterModel.java
@@ -1,7 +1,6 @@
 package com.ita.edu.speakua.ui.Pages.CenterPO;
 
 import com.ita.edu.speakua.ui.BasePO;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -17,10 +16,14 @@ public class AddCenterModel extends BasePO {
 
     @FindBy(xpath = "//button[@class='ant-btn ant-btn-default next-btn']")
     private WebElement nextStepButton;
-
-
     @FindBy(xpath = "//span[text()='New Center Name']")
     private WebElement selectCenterLocation;
+    @FindBy(xpath = "//div[1]//label[1]")
+    private WebElement scroll;
+
+    @FindBy(xpath = "//div[contains(@class, 'ant-checkbox-group location-list')]/div[last()]/label/span/input")
+    private WebElement newLocation;
+
 
     public AddCenterModel(WebDriver driver) {
         super(driver);
@@ -37,12 +40,10 @@ public class AddCenterModel extends BasePO {
     }
 
     public AddCenterModel selectCenterLocation() {
-        WebElement scroll = driver.findElement(By.xpath("//div[1]//label[1]"));
         scroll.click();
         scroll.click();
         scroll.sendKeys(Keys.END);
-        WebElement location = driver.findElement(By.xpath("//div[contains(@class, 'ant-checkbox-group location-list')]/div[last()]/label/span/input"));
-        location.click();
+        newLocation.click();
         return this;
     }
 
