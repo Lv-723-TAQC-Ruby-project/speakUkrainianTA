073340c94f09d0d325bac6d549a978555d33f306
sofiy <sofiya.ometiukh@gmail.com>
2022-12-21 20:15:13 +0200

    fixed clubTests

--- src/test/java/com/ita/edu/speakua/ui/ClubTests.java
+++ src/test/java/com/ita/edu/speakua/ui/ClubTests.java
@@ -4,6 +4,7 @@ import com.ita.edu.speakua.ui.Pages.ClubsPO.AddClubModal;
 import com.ita.edu.speakua.ui.Pages.ClubsPO.ClubPage;
 import com.ita.edu.speakua.ui.runners.BaseTestRunner;
 import org.testng.Assert;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
