1a2ef061e1693e6ba16b968f72c3dd8b640f2982
Edgers <a.b.23042000@gmail.com>
2022-12-23 00:25:17 +0200

    Working on tests (haha)

--- src/test/java/com/ita/edu/speakua/ui/ClubTests.java
+++ src/test/java/com/ita/edu/speakua/ui/ClubTests.java
@@ -76,6 +76,10 @@ public class ClubTests extends BaseTestRunner {
                 "Вибір лише за вами, батьки....").isErrorMessageDisplayed("Опис гуртка може містити від 40 до 1500 символів."));
         //TUA-178
         softAssert.assertTrue(addClubModal.enterClubDescription("ё ы э ъ").isErrorMessageDisplayed("Опис гуртка не може містити російські літери"));
+        //TUA-172
+        softAssert.assertTrue(addClubModal.enterClubDescription("s".repeat(1000)).successMessageDisplayed());
+        softAssert.assertTrue(addClubModal.enterClubDescription("s".repeat(40)).successMessageDisplayed());
+        softAssert.assertTrue(addClubModal.enterClubDescription("s".repeat(1500)).successMessageDisplayed());
         //TUA-173
         softAssert.assertTrue(addClubModal.enterClubDescription("'Education', 'students', 'Школа' 'балету'").successMessageDisplayed());
         softAssert.assertTrue(addClubModal.enterClubDescription("1234567890123456789012345678901234567890").successMessageDisplayed());
@@ -137,11 +141,6 @@ public class ClubTests extends BaseTestRunner {
         Assert.assertEquals(checkInformationAboutCenterByNumber, "0672222222");
         boolean checkInformationAboutCenterByDescription = new ClubPage(driver).isDescriptionAboutCenter("Тестовий гурток для додавання центру Тестовий гурток для додавання центру");
         Assert.assertTrue(checkInformationAboutCenterByDescription);
-
-
-
-
-
 
 
     }
