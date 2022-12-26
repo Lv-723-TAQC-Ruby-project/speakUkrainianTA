cd30d8e0c5306c3887364a6f83960d2985310099
merykul <mariakulv@gmail.com>
2022-12-26 14:50:45 +0300

    fixed ChangePasswordNegativeTests

--- src/main/java/com/ita/edu/speakua/ui/Pages/ProfilePO/EditProfileModal.java
+++ src/main/java/com/ita/edu/speakua/ui/Pages/ProfilePO/EditProfileModal.java
@@ -18,24 +18,24 @@ public class EditProfileModal extends BasePO {
     @FindBy(xpath = "//input[@id='edit_phone']")
     private WebElement editNumberPhoneField;
 
-    @FindBy(xpath = "//*[@id=\"edit_currentPassword\"]")
+    @FindBy(xpath = "//*[@id='edit_currentPassword']")
     private WebElement currentPasswordField;
 
-    @FindBy(xpath = "//*[@id=\"edit_password\"]")
+    @FindBy(xpath = "//*[@id='edit_password']")
     private WebElement newPasswordField;
 
-    @FindBy(xpath = "//*[@id=\"edit\"]/div[2]/div[10]/button")
+    @FindBy(xpath = "//*[@id='edit']/div[2]/div[10]/button")
     private WebElement saveChangesButton;
 
     @FindBy(xpath = "//div[@class='ant-form-item-explain-error']")
     private WebElement messageAboutIncorrectlyEnteredLastName;
 
-    @FindBy(xpath = "//*[@id=\"edit_currentPassword_help\"]/div")
+    @FindBy(xpath = "//*[@id='edit_currentPassword_help']/div")
     private WebElement emptyCurrentPasswordFieldMessage;
 
-    @FindBy(xpath = "//*[@id=\"edit_password_help\"]/div")
+    @FindBy(xpath = "//*[@id='edit_password_help']/div")
     private WebElement emptyNewPasswordFieldMessage;
-    @FindBy(xpath = "//*[@id=\"edit_confirmPassword_help\"]/div")
+    @FindBy(xpath = "//*[@id='edit_confirmPassword_help']/div")
     private WebElement emptyConfirmPasswordMessage;
 
     @FindBy(xpath = "//div[2]/div[7]/div/input")
@@ -47,6 +47,7 @@ public class EditProfileModal extends BasePO {
 
     @Step("Click Change password check box")
     public EditProfileModal clickChangePasswordCheckBox() {
+        sleep(5);
         changePasswordCheckBox.click();
         return this;
     }
@@ -108,19 +109,20 @@ public class EditProfileModal extends BasePO {
 
     @Step("Enter current password")
     public EditProfileModal enterCurrentPassword(String password) {
-        currentPasswordField.sendKeys(Keys.ENTER);
+        sleep(5);
+        currentPasswordField.sendKeys(password);
         return this;
     }
 
     @Step("Enter new password")
     public EditProfileModal enterNewPasswordInTheNewPasswordField(String newPassword) {
-        newPasswordField.sendKeys(Keys.ENTER);
+        newPasswordField.sendKeys(newPassword);
         return this;
     }
 
     @Step("Enter new password in the Confirm new password field")
     public EditProfileModal enterConfirmPassword(String password) {
-        currentPasswordField.sendKeys(Keys.ENTER);
+        currentPasswordField.sendKeys(password);
         return this;
     }
 
