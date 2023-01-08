a62a1f35249a1b53be03706cc5679eba725fe6f4
merykul <mariakulv@gmail.com>
2023-01-08 23:13:07 +0300

    created ClubsService

---
+++ src/main/java/com/ita/edu/speakua/jdbc/services/ClubsService.java
@@ -0,0 +1,19 @@
+package com.ita.edu.speakua.jdbc.services;
+
+import com.ita.edu.speakua.jdbc.dao.ClubsDTO;
+import com.ita.edu.speakua.jdbc.entity.ClubsEntity;
+
+import java.util.List;
+
+public class ClubsService {
+    private final ClubsDTO clubsDTO;
+
+    public ClubsService() {
+        this.clubsDTO = new ClubsDTO();
+    }
+
+    public List<ClubsEntity> getAll() {
+        return clubsDTO.selectAll();
+    }
+
+}
