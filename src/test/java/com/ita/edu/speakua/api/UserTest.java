e5f89f41ba056584b2540f8acd916921084b5d7d
Nata Kmitiuk <lovelynata9@gmail.com>
2023-01-25 13:53:10 +0200

    fixed API tests

--- src/test/java/com/ita/edu/speakua/api/UserTest.java
+++ src/test/java/com/ita/edu/speakua/api/UserTest.java
@@ -31,66 +31,36 @@ public class UserTest {
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(response.getStatus(),400);
         softAssert.assertEquals(response.getMessage(),"\"firstName\" can`t contain numbers");
-        softAssert.assertAll();
-        User requestBody1 = new User(203,
-                "soyec48727@busantei.com",
-                "NastiaNastiaNastiaNastiaNastia",
-                "Kukh",
-                "999999922",
-                "ROLE_MANAGER",
-                "",
-                true);
-        ErrorResponse response1 = client.badPut(requestBody1);
-        softAssert.assertEquals(response1.getStatus(),400);
-        softAssert.assertEquals(response1.getMessage(),"\"firstName\" can contain from 1 to 25 letters");
-        softAssert.assertAll();
-        User requestBody2 = new User(203,
-                "soyec48727@busantei.com",
-                "Nastia!@##$#$%",
-                "Kukh",
-                "999999922",
-                "ROLE_MANAGER",
-                "",
-                true);
-        ErrorResponse response2 = client.badPut(requestBody2);
-        softAssert.assertEquals(response2.getStatus(),400);
-        softAssert.assertEquals(response2.getMessage(),"\"firstName\" can contain only ukrainian and english letters");
-        softAssert.assertAll();
-        User requestBody3 = new User(203,
-                "soyec48727@busantei.com",
-                "Nastia",
-                "Kukhar#%$#",
-                "999999922",
-                "ROLE_MANAGER",
-                "",
-                true);
-        ErrorResponse response3 = client.badPut(requestBody3);
-        softAssert.assertEquals(response3.getStatus(),400);
-        softAssert.assertEquals(response3.getMessage(),"\"lastName\" can contain only ukrainian and english letters");
-        softAssert.assertAll();
-        User requestBody4 = new User(203,
-                "soyec48727@busantei.com",
-                "Nastia",
-                "KukharKukharKukharKukharKukharKukharKukhar#",
-                "999999922",
-                "ROLE_MANAGER",
-                "",
-                true);
-        ErrorResponse response4 = client.badPut(requestBody4);
-        softAssert.assertEquals(response4.getStatus(),400);
-        softAssert.assertEquals(response4.getMessage(),"\"lastName\" can contain from 1 to 25 letters");
-        softAssert.assertAll();
-        User requestBody5 = new User(203,
-                "soyec48727@busantei.com",
-                "Nastia",
-                "Kukhar123343#",
-                "999999922",
-                "ROLE_MANAGER",
-                "",
-                true);
-        ErrorResponse response5 = client.badPut(requestBody5);
-        softAssert.assertEquals(response5.getStatus(),400);
-        softAssert.assertEquals(response5.getMessage(),"\"lastName\" can`t contain numbers");
+
+        User user = requestBody.toBuilder().build();
+        user.setFirstName("NastiaNastiaNastiaNastiaNastia");
+        response = client.badPut(user);
+        softAssert.assertEquals(response.getStatus(),400);
+        softAssert.assertEquals(response.getMessage(),"\"firstName\" can contain from 1 to 25 letters");
+
+        user = requestBody.toBuilder().build();
+        user.setFirstName("Nastia!@##$#$%");
+        response = client.badPut(user);
+        softAssert.assertEquals(response.getStatus(),400);
+        softAssert.assertEquals(response.getMessage(),"\"firstName\" can contain only ukrainian and english letters");
+
+        user = requestBody.toBuilder().build();
+        user.setLastName("Kukhar#%$#");
+        response = client.badPut(user);
+        softAssert.assertEquals(response.getStatus(),400);
+        softAssert.assertEquals(response.getMessage(),"\"lastName\" can contain only ukrainian and english letters");
+
+        user = requestBody.toBuilder().build();
+        user.setLastName("KukharKukharKukharKukharKukharKukharKukhar#");
+        response = client.badPut(user);
+        softAssert.assertEquals(response.getStatus(),400);
+        softAssert.assertEquals(response.getMessage(),"\"lastName\" can contain from 1 to 25 letters");
+
+        user = requestBody.toBuilder().build();
+        user.setLastName("Kukhar123343#");
+        response = client.badPut(user);
+        softAssert.assertEquals(response.getStatus(),400);
+        softAssert.assertEquals(response.getMessage(),"\"lastName\" can`t contain numbers");
         softAssert.assertAll();
 
     }
