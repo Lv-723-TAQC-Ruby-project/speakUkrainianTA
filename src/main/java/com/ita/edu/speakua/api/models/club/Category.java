1f4419de4dfe2605e41388b055fdedabc10bc70d
simpleUA <a.lipskiy@yahoo.com>
2023-01-28 18:06:07 +0300

    Add files via upload

---
+++ src/main/java/com/ita/edu/speakua/api/models/club/Category.java
@@ -0,0 +1,21 @@
+package com.ita.edu.speakua.api.models.club;
+
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Category {
+    private int id;
+    private int sortby;
+    private String name;
+    private String description;
+    private String urlLogo;
+    private String backgroundColor;
+    private String tagBackgroundColor;
+    private String tagTextColor;
+
+    private List<Club> clubs;
+
+}
---
+++ src/main/java/com/ita/edu/speakua/api/models/club/LocationResponse.java
@@ -0,0 +1,22 @@
+package com.ita.edu.speakua.api.models.club;
+
+import lombok.Data;
+@Data
+public class LocationResponse {
+    private int id;
+    private String name;
+    private String address;
+    private String cityName;
+    private String districtName;
+    private String stationName;
+    private LocationCity locationCity;
+    private int cityId;
+    private int districtId;
+    private int stationId;
+    private int clubId;
+    private String coordinates;
+    private double latitude;
+    private double longitude;
+    private String phone;
+//    private Double key;
+}
