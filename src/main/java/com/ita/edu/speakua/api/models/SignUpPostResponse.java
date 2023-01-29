1747cdececb1422a49d335ff178fce2399be6ee5
Stanislav Kovalov <stas.kovalyov16@gmail.com>
2023-01-29 21:43:20 +0200

    added registration test

---
+++ src/main/java/com/ita/edu/speakua/api/models/SignUpPostResponse.java
@@ -0,0 +1,10 @@
+package com.ita.edu.speakua.api.models;
+
+import lombok.Data;
+
+@Data
+public class SignUpPostResponse {
+    private int id;
+    private String email;
+    private String roleName;
+}
