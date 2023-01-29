2e8849e51cdca4911fdfbb4da856d1540cc9b0da
simpleUA <a.lipskiy@yahoo.com>
2023-01-29 12:28:13 +0200

    added "Verify that User as 'Керiвник гуртка' cannot create new club is in a center if 'Назва' field contain more than 100 characters" test

--- src/test/java/com/ita/edu/speakua/api/ClubTest.java
+++ src/test/java/com/ita/edu/speakua/api/ClubTest.java
@@ -10,11 +10,15 @@ import com.ita.edu.speakua.api.models.club.ClubPostResponse;
 import com.ita.edu.speakua.api.models.club.Location;
 import com.ita.edu.speakua.utils.ConfigProperties;
 import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
 import io.restassured.response.Response;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+import org.testng.reporters.Files;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +36,7 @@ public class ClubTest {
     }
 
 
-     @Description("Successful creating club")
+    @Description("Successful creating club")
     @Test
     public void successfulPost() {
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
@@ -135,5 +139,54 @@ public class ClubTest {
         softAssert.assertEquals(response.getStatus(), 400);
         softAssert.assertEquals(response.getMessage(), "name Довжина назви має бути від 5 до 100 символів");
         softAssert.assertAll();
+    }
+
+
+    @Description("TUA-503 - Verify that User as 'Керiвник гуртка' cannot create new club is in a center if 'Назва' field contain more than 100 characters")
+    @Test
+    public void verifyThatUserCannotCreateNewClubIfNameContainMore100Characters() {
+        List<String> categoriesName = new ArrayList<String>() {
+            {
+                add("Вокальна студія, музика, музичні інструменти");
+            }
+        };
+        Location location = Location
+                .builder()
+                .name("Голосівська")
+                .cityName("Київ")
+                .districtName("Голосіївський")
+                .stationName("Голосіївська")
+                .address("https://speak-ukrainian.org.ua/dev/club/910")
+                .coordinates("50.35535081747696, 30.51765754176391")
+                .phone("0937777777")
+                .key(0.00)
+                .build();
+
+        List<Location> locations = new ArrayList<Location>() {
+            {
+                add(location);
+            }
+        };
+        ClubPostRequest requestBody = new ClubPostRequest(
+                "Ми поставили перед собою ціль створити мережу найкращих центрів раннього розвитку в Україні, де дітки навчатимуться з задоволенням, а батьки радітимуть від результатів",
+                "{\"blocks\":[{\"key\":\"brl63\",\"text\":\"аааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа\",\"type\":\"unstyled\",\"depth\":1,\"inlineStyleRanges\":[],\"entityRanges\":[],\"data\":{}}],\"entityMap\":{}}",
+                2,
+                categoriesName,
+                locations,
+                2,
+                18,
+                null,
+                null,
+                true,
+                "{\"1\"::\"ліл\"}",
+                264
+        );
+        Response response = this.client.successPost(requestBody);
+        ErrorResponse errorResponse = response.as(ErrorResponse.class);
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(response.statusCode(), 400);
+        softAssert.assertEquals(errorResponse.getMessage(), "name Довжина назви має бути від 5 до 100 символів");
+        softAssert.assertAll();
+
     }
 }
